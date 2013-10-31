6f2819246bc58e22ec6e38979ae948891d4a6476
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-10-31 13:13:49 +0800

    change CMD_BUF_SIZE & PIPE_BUF
    Length of atomic output message is limitted to PIPE_BUF - sizeof(int),
    because a field storing length uses sizeof(int) bytes in message queue.

--- kernel.c
+++ kernel.c
@@ -65,13 +65,13 @@ void puts(char *s)
 #define MAX_ARGC 19
 #define MAX_CMDHELP 1023
 #define HISTORY_COUNT 20
-#define CMDBUF_SIZE 100
+#define CMDBUF_SIZE 40
 #define MAX_ENVCOUNT 30
 #define MAX_ENVNAME 15
 #define MAX_ENVVALUE 127
 #define STACK_SIZE 512 /* Size of task stacks in words */
 #define TASK_LIMIT 8  /* Max number of tasks we can handle */
-#define PIPE_BUF   64 /* Size of largest atomic pipe message */
+#define PIPE_BUF   128 /* Size of largest atomic pipe message */
 #define PATH_MAX   32 /* Longest absolute path */
 #define PIPE_LIMIT (TASK_LIMIT * 2)
 
